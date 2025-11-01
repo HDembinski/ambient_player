b5af201241d96c302b0381dce7dc132b2388f76b
Hans Dembinski <hans.dembinski@gmail.com>
2025-11-01 14:54:55 +0100

    u

--- main.py
+++ main.py
@@ -1,62 +1,48 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
 import threading
 import vlc  # using python-vlc for streaming
 from pathlib import Path
-import subprocess as subp
 
 app = FastAPI()
 
 # URL of the nature-sounds stream
 STREAM_URL = "http://ec3.yesstreaming.net:3540/stream"
-THIS_DIR = Path(__file__).parent
-
-
-def command_result(cmd: str) -> str:
-    return subp.run(cmd.split(), text=True, stdout=subp.PIPE).stdout.strip()
-
-
-def get_bluetooth_device():
-    return command_result("bluetoothctl devices Paired").split()[1]
+THIS_PATH = Path(__file__).parent
+MEDIA_PATH = THIS_PATH / "media"
 
 
 state_lock = threading.Lock()
-is_playing = False
-player = vlc.MediaPlayer(STREAM_URL)
-bluetooth_device = get_bluetooth_device()
-
-
-def start_stream():
-    player.play()
-
-
-def stop_stream():
-    player.stop()
+player = vlc.MediaListPlayer()
+player.set_playback_mode(vlc.PlaybackMode.loop)
+media_list = vlc.MediaList()
+for p in MEDIA_PATH.glob("*.mp3"):
+    media_list.add_media(p)
+player.set_media_list(media_list)
 
 
 # Serve static files
 @app.get("/")
-async def index() -> FileResponse:
-    return FileResponse((THIS_DIR / "static/index.html"), media_type="text/html")
+async def index() -> HTMLResponse:
+    with open(THIS_PATH / "static/index.html", encoding="utf-8") as f:
+        return HTMLResponse(f.read())
 
 
 @app.get("/state")
 async def get_state() -> bool:
     with state_lock:
-        return is_playing
+        return player.is_playing()
 
 
 @app.post("/toggle")
 async def toggle_playback() -> bool:
-    global is_playing
     with state_lock:
-        if is_playing:
-            stop_stream()
-            is_playing = False
+        if player.is_playing():
+            player.pause()
+            return False
         else:
-            start_stream()
-            is_playing = True
-        return is_playing
+            player.play()
+            return True
 
 
 if __name__ == "__main__":
